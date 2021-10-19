898a6439d04e67307834c78a46e5164d4d4ab653
dedesaepulloh <dedesaepulloh77@gmail.com>
2021-10-19 13:31:27 +0700

    add extension properties

--- src/main/kotlin/app/ExtensionFunction.kt
+++ src/main/kotlin/app/ExtensionFunction.kt
@@ -2,8 +2,10 @@ package app
 
 import data.Student
 import data.sayHello
+import data.upperName
 
 fun main() {
     val student: Student? = Student("Dede", 23)
     student.sayHello("Rahmat")
+    println(student?.upperName)
 }
